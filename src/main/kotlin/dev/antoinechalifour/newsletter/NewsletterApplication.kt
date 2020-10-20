1cd3f63c8a2dca628d2eb3cf678904b0535b94d8
Antoine Chalifour <antoine.chalifour@gmail.com>
2020-10-20 19:55:30 +0200

    feat: improve emails (#2)

--- src/main/kotlin/dev/antoinechalifour/newsletter/NewsletterApplication.kt
+++ src/main/kotlin/dev/antoinechalifour/newsletter/NewsletterApplication.kt
@@ -5,10 +5,16 @@ import dev.antoinechalifour.newsletter.infrastructure.RssService
 import org.simplejavamail.springsupport.SimpleJavaMailSpringSupport
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Import
 import org.springframework.scheduling.annotation.EnableScheduling
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect
+import org.thymeleaf.spring5.SpringTemplateEngine
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver
+import org.thymeleaf.templatemode.TemplateMode
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver
 import retrofit2.Retrofit
 import retrofit2.converter.simplexml.SimpleXmlConverterFactory
 import java.time.Clock
@@ -35,6 +41,20 @@ class NewsletterApplication {
 
     @Bean
     fun clock() = Clock.system(ZoneId.of("Europe/Paris"))
+
+    @Bean
+    fun emailTemplateEngine() = SpringTemplateEngine().apply {
+        addDialect(Java8TimeDialect())
+        setTemplateResolver(emailTemplateResolver())
+    }
+
+    private fun emailTemplateResolver() = ClassLoaderTemplateResolver().apply {
+        prefix = "emails/"
+        suffix = ".html"
+        isCacheable = false
+        templateMode = TemplateMode.HTML
+        characterEncoding = "UTF-8"
+    }
 }
 
 fun main(args: Array<String>) {
